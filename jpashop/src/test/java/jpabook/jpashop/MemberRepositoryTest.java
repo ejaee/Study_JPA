a1e8182e50d986c60a12f4f6204b59f258646625
choiejae <ejae8319@gmail.com>
2023-08-31 18:55:57 +0900

    fix: wrong run... because of test file

--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -8,8 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-
-@RunWith(SpringRunner.class) // Spring 과 관련된 것들을 테스트 할 것이라고 알려준다.
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class MemberRepositoryTest {
 
